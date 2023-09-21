0fce15a051ff7d3bffacfe1ac537a110b81e4109
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-09-21 13:44:56 -0500

    Initial Commit

---
+++ src/main/java/com/duberlyguarnizo/accountinquireservice/AccountInquireServiceApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.accountinquireservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AccountInquireServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountInquireServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/duberlyguarnizo/accountinquireservice/AccountInquireServiceApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.accountinquireservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountInquireServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
